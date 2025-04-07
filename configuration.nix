bde85d119eb998dbd491fdc2975d97d80782ef64
typovrak <typovrak@gmail.com>
2025-04-08 00:05:09 +0200

    first config trying

---
+++ configuration.nix
@@ -0,0 +1,13 @@
+{ config, pkgs, ... }:
+
+let {
+	username = "typovrak";
+	group = config.users.users.${username}.group or "users";
+	home = config.users.users.${username}.home;
+} in {
+	system.activationScripts.projects = ''
+		mkdir -p ${home}/projects
+		chown ${username}:${group} ${home}/projects
+		chmod 755 ${home}/projects
+	'';
+}
